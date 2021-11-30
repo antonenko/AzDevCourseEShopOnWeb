27f7920fdfb5e1dde44be5915e327236e065dfac
Aliaksandr Antonenka <Aliaksandr_Antonenka@epam.com>
2021-11-30 14:17:13 +0100

    Add services to send order details to service bus

---
+++ src/ApplicationCore/Entities/ReservedOrderDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace Microsoft.eShopWeb.ApplicationCore.Entities
+{
+    public class ReservedOrderDetails
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+    }
+}
