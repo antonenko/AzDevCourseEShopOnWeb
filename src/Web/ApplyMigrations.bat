d0ec99dc7f457a08ca2dc69c61b75b496cde1504
Aliaksandr Antonenka <Aliaksandr_Antonenka@epam.com>
2021-11-30 12:09:58 +0100

    Add project files.

---
+++ src/Web/ApplyMigrations.bat
@@ -0,0 +1,4 @@
+dotnet restore
+dotnet tool restore
+dotnet ef database update -c catalogcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
+dotnet ef database update -c appidentitydbcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
